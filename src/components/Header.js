2a097703f45f771e90da6eb253822777b9486903
Oscar Zambrano <oscarz.developer@gmail.com>
2018-10-10 15:36:41 -0400

    Stateless Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Header extends Component {
-  render() {
-    const { tagline } = this.props;
-
-    return (
-      <header className="top">
-        <h1>
-          Catch
-          <span className="ofThe">
-            <span className="of">Of</span>
-            <span className="the">The</span>
-          </span>
-          Day
-        </h1>
-        <h3 className="tagline">
-          <span>{tagline}</span>
-        </h3>
-      </header>
-    );
-  }
-}
+const Header = ({ tagline }) => (
+  <header className="top">
+    <h1>
+      Catch
+      <span className="ofThe">
+        <span className="of">Of</span>
+        <span className="the">The</span>
+      </span>
+      Day
+    </h1>
+    <h3 className="tagline">
+      <span>{tagline}</span>
+    </h3>
+  </header>
+)
 
 Header.propTypes = {
   tagline: PropTypes.string,
