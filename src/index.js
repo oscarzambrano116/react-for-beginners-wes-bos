61db01c0b638ee7bbe0569790d5427fe606c88fc
Oscar Zambrano <oscarz.developer@gmail.com>
2018-10-10 15:10:51 -0400

    Creating Header, Inventory and Order components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
+import App from './components/App';
 import './css/style.css';
 
 const app = document.querySelector('#main');
-render(<StorePicker />, app);
+render(<App />, app);
