375cbf6cf13ca89bc2e6a1a145059484910786e5
Oscar Zambrano <oscarz.developer@gmail.com>
2018-10-10 15:46:11 -0400

    Adding Router component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import StorePicker from './components/StorePicker';
-import App from './components/App';
+import Router from './components/Router';
 import './css/style.css';
 
 const app = document.querySelector('#main');
-render(<App />, app);
+render(<Router />, app);
